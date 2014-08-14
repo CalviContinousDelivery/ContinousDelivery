0608ed41aef0ab90884799c4bb7ffb79b2b5b517
gautamvbaindur <g.baindur@prowareness.nl>
2014-08-14 15:06:11 +0530

    Added 2 new projects

---
+++ CustomLibraryTest/MathsLibraryTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomLibrary;
+
+namespace CustomLibraryTest
+{
+    [TestClass]
+    public class MathsLibraryTest
+    {
+        private MathsLibrary math;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            math = new MathsLibrary();
+        }
+
+        [TestMethod]
+        public void Add_Test()
+        {
+            Assert.AreEqual(7, math.Add(5, 2));
+        }
+
+        [TestMethod]
+        public void Subtract_Test()
+        {
+            Assert.AreEqual(3, math.Subtract(5, 2));
+        }
+
+        [TestMethod]
+        public void Multiply_Test()
+        {
+            Assert.AreEqual(10, math.Multiply(5, 2));
+        }
+
+    }
+}
