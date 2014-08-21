17c8263efbb378fcfa65e9012bef411e589df3e6
calvicontinousdelivery <g.baindur@prowareness.nl>
2014-08-21 17:29:58 +0530

    reduce code coberage

--- CustomLibrary/MathsLibrary.cs
+++ CustomLibrary/MathsLibrary.cs
@@ -22,6 +22,18 @@ namespace CustomLibrary
         public int Multiply(int p1, int p2)
         {
             return p1 * p2;
-        }        
+        }
+
+        public int Divide()
+        {
+            string s = string.Empty;
+            return 0;
+        }
+
+        public int Divide2()
+        {
+            string s = string.Empty;
+            return 0;
+        }
     }
 }
