1adb6dd6c4328110f55335c4427ebb94c6599f42
calvicontinousdelivery <g.baindur@prowareness.nl>
2014-09-02 13:55:46 +0530

    added change to trigger CI

--- CustomLibraryTest/MathsLibraryTest.cs
+++ CustomLibraryTest/MathsLibraryTest.cs
@@ -33,6 +33,7 @@ namespace CustomLibraryTest
         [TestCategory("Gautam")]
         public void Multiply_Test()
         {
+            //idhfosdhfuihadou
             Assert.AreEqual(10, math.Multiply(5, 2));
         }
 
